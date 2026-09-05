6778fad7a0e861b89a7c6788eb89560613dd6747
Vadym <vadym@example.com>
2026-09-05 21:06:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from os import mkdir, remove
+
+
+def move_file(command: str) -> None:
+    com, old_file, new_file = command.split(" ")
+    path = new_file.split("/")
+    current_path = ""
+    for i in range(len(path) - 1):
+        try:
+            mkdir(f"{current_path}{path[i]}")
+        except FileExistsError:
+            continue
+        finally:
+            current_path += f"{path[i]}/"
+    with (open(old_file, "r")) as old, (open(new_file, "w")) as new:
+        new.write(old.read())
+    remove(old_file)
